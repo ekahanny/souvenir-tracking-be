5fd2be4415da72e263050b35752d90eb0c637189
ekahanny <ekahnny@gmail.com>
2025-08-15 17:13:58 +0800

    fix: bcrypt

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,5 +1,5 @@
 import User from "../models/UserModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
 import mongoose from "mongoose";
